737c761af720d3a1dbb4435d76185cb782917e2a
vashineyu <vashineyu@gmail.com>
2018-10-23 08:24:17 +0000

    update policy02

--- policy_02/pong_utils_tf.py
+++ policy_02/pong_utils_tf.py
@@ -6,7 +6,7 @@ from JSAnimation.IPython_display import display_animation
 from matplotlib import animation
 from IPython.display import display
 import random as rand
-
+import tensorflow as tf
 
 RIGHT=4
 LEFT=5 
@@ -27,7 +27,7 @@ def preprocess_batch(images, bkg_color = np.array([144, 72, 17])):
     list_of_images_prepro = np.mean(list_of_images[:,:,34:-16:2,::2]-bkg_color,
                                     axis=-1)/255.
     batch_input = np.swapaxes(list_of_images_prepro,0,1)
-    return batch_input
+    return np.rollaxis(batch_input, 1,4)
 
 
 # function to animate a list of frames
@@ -51,11 +51,13 @@ def play(env, policy, time=2000, preprocess=None, nrand=5):
 
     # star game
     env.step(1)
+    reward = []
     
     # perform nrand random steps in the beginning
     for _ in range(nrand):
         frame1, reward1, is_done, _ = env.step(np.random.choice([RIGHT,LEFT]))
         frame2, reward2, is_done, _ = env.step(0)
+        reward.append(reward1+reward2)
     
     anim_frames = []
     
@@ -66,8 +68,11 @@ def play(env, policy, time=2000, preprocess=None, nrand=5):
         
         # RIGHT = 4, LEFT = 5
         action = RIGHT if rand.random() < prob else LEFT
-        frame1, _, is_done, _ = env.step(action)
-        frame2, _, is_done, _ = env.step(0)
+        frame1, re1, is_done, _ = env.step(action)
+        frame2, re2, is_done, _ = env.step(0)
+        
+        reward.append(re1 + re2)
+        
 
         if preprocess is None:
             anim_frames.append(frame1)
@@ -81,7 +86,6 @@ def play(env, policy, time=2000, preprocess=None, nrand=5):
     
     animate_frames(anim_frames)
     return 
-
 
 
 # collect trajectories for a parallelized parallelEnv object
@@ -109,19 +113,12 @@ def collect_trajectories(envs, policy, tmax=200, nrand=5):
     for t in range(tmax):
 
         # prepare the input
-        # preprocess_batch properly converts two frames into 
-        # shape (n, 2, 80, 80), the proper input for the policy
-        # this is required when building CNN with pytorch
+        # preprocess_batch properly converts two frames
         batch_input = preprocess_batch([fr1,fr2])
-        
-        # probs will only be used as the pi_old
-        # no gradient propagation is needed
-        # so we move it to the cpu
-        probs = policy(batch_input).squeeze().cpu().detach().numpy()
+        probs = policy.act(batch_input)
         
         action = np.where(np.random.rand(n) < probs, RIGHT, LEFT)
         probs = np.where(action==RIGHT, probs, 1.0-probs)
-        
         
         # advance the game (0=no action)
         # we take one action and skip game forward
@@ -143,49 +140,7 @@ def collect_trajectories(envs, policy, tmax=200, nrand=5):
 
 
     # return pi_theta, states, actions, rewards, probability
-    return prob_list, state_list, \
-        action_list, reward_list
-
-# convert states to probability, passing through the policy
-def states_to_prob(policy, states):
-    states = torch.stack(states)
-    policy_input = states.view(-1,*states.shape[-3:])
-    return policy(policy_input).view(states.shape[:-3])
-
-# return sum of log-prob divided by T
-# same thing as -policy_loss
-def surrogate(policy, old_probs, states, actions, rewards,
-              discount = 0.995, beta=0.01):
-
-    discount = discount**np.arange(len(rewards))
-    rewards = np.asarray(rewards)*discount[:,np.newaxis]
-    
-    # convert rewards to future rewards
-    rewards_future = rewards[::-1].cumsum(axis=0)[::-1]
-    
-    mean = np.mean(rewards_future, axis=1)
-    std = np.std(rewards_future, axis=1) + 1.0e-10
-
-    rewards_normalized = (rewards_future - mean[:,np.newaxis])/std[:,np.newaxis]
-    
-    # convert everything into pytorch tensors and move to gpu if available
-    actions = torch.tensor(actions, dtype=torch.int8, device=device)
-    old_probs = torch.tensor(old_probs, dtype=torch.float, device=device)
-    rewards = torch.tensor(rewards_normalized, dtype=torch.float, device=device)
-
-    # convert states to policy (or probability)
-    new_probs = states_to_prob(policy, states)
-    new_probs = torch.where(actions == RIGHT, new_probs, 1.0-new_probs)
-
-    ratio = new_probs/old_probs
-
-    # include a regularization term
-    # this steers new_policy towards 0.5
-    # add in 1.e-10 to avoid log(0) which gives nan
-    entropy = -(new_probs*torch.log(old_probs+1.e-10)+ \
-        (1.0-new_probs)*torch.log(1.0-old_probs+1.e-10))
-
-    return torch.mean(ratio*rewards + beta*entropy)
+    return prob_list, state_list, action_list, reward_list
 
     
 # clipped surrogate function
@@ -233,33 +188,4 @@ def clipped_surrogate(policy, old_probs, states, actions, rewards,
     # averaged over time-step and number of trajectories
     # this is desirable because we have normalized our rewards
     return torch.mean(clipped_surrogate + beta*entropy)
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-class Policy(nn.Module):
-
-    def __init__(self):
-        super(Policy, self).__init__()
-        # 80x80x2 to 38x38x4
-        # 2 channel from the stacked frame
-        self.conv1 = nn.Conv2d(2, 4, kernel_size=6, stride=2, bias=False)
-        # 38x38x4 to 9x9x32
-        self.conv2 = nn.Conv2d(4, 16, kernel_size=6, stride=4)
-        self.size=9*9*16
-        
-        # two fully connected layer
-        self.fc1 = nn.Linear(self.size, 256)
-        self.fc2 = nn.Linear(256, 1)
-
-        # Sigmoid to 
-        self.sig = nn.Sigmoid()
-        
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = x.view(-1,self.size)
-        x = F.relu(self.fc1(x))
-        return self.sig(self.fc2(x))
     
